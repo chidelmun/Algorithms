cc05b717fd8e65695d111d73326684d3ee445918
Delveri Munang <chickdelveri@gmail.com>
2015-07-19 22:18:14 +0100

    Stack Interface

---
+++ stack.h
@@ -0,0 +1,17 @@
+/*
+Here is an Implementation of the Stack Data structure
+Feel free to use this code and modify it to meet your needs
+@Author: Delveri Munang
+@Date  : 19/07/2015
+*/
+
+template <typename T> class stack{
+private:
+	int size;
+
+
+public:
+	void pop();
+	int puch(T item);
+
+};
