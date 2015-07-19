5853d0b5cdc39776a37160a55e68321f7ec446e0
Delveri Munang <chickdelveri@gmail.com>
2015-07-19 22:31:49 +0100

    Stack Interface review

--- stack.h
+++ stack.h
@@ -5,13 +5,21 @@ Feel free to use this code and modify it to meet your needs
 @Date  : 19/07/2015
 */
 
-template <typename T> class stack{
-private:
-	int size;
+template <typename T>
+class stack{
+	private:
+		int size;
+		int top;
+		T *elements;
 
 
-public:
-	void pop();
-	int puch(T item);
+	public:
+		stack();
+		stack(int size);
+		stack(int size, T elmt);
+		void pop();
+		void puch(T item);
+		int peek();
+		bool isEmpty();
 
 };
