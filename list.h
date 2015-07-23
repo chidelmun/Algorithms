1ce0d70ab122ee82e50c27fc97a4f17669ed3fa5
Delveri Munang <chickdelveri@gmail.com>
2015-07-23 13:33:34 +0100

    Linkrd List Interface

---
+++ list.h
@@ -0,0 +1,24 @@
+/*Here is an inmplementation of the singly linked list. This Implementation is
+non generic and supports Integers only. Feel free to use the container 
+ in your projects modifying it to meet your needs
+@file 	:list.h
+@Author	:Delveri Munang
+@Date 	:21/07/2015
+@Project:Data structures and Algorithms in C++
+		 University of Buea, Cameroon.
+
+ */
+
+struct node{
+	int value;
+	struct node* next;
+};
+
+class list{
+private:
+	node datum;
+public:
+	list();
+	
+
+};
