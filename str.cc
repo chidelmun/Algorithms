b0c1384016f3e06c5b3bfdd027c07483185fb5aa
Delveri Munang <chickdelveri@gmail.com>
2015-08-09 11:22:38 +0100

    Did refactoring and cleanup

--- str.cc
+++ str.cc
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <string>
 #include "str.h"
 
 int  main(int argc, char const *argv[])
 {
 	str name("Dario Garnett");
+	string user("Chidelmun");
 
-	cout << name.length() << endl\
-		 << "Printing .." << name[6]\
-		 << endl;;
+	cout << name.length() << endl     \
+		 << "Printing .." << name[atoi(argv[1])] << endl \
+		 << "Printing .." << user[-5] \
+		 << endl;
 	/* code */
 	return 0;
 }
