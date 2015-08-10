6d9fdfca95dccaec22fc95e7b10fd0d6d323e5cf
Delveri Munang <chickdelveri@gmail.com>
2015-08-10 08:46:19 +0100

    Descriptors for trees

--- trees.hh
+++ trees.hh
@@ -1,22 +1,23 @@
+/*Binary search tree Interface*/
 #ifndef _BTREE_
 #define _BTREE_
 
 
 
 #endif
-
+/*Red Black Tree Interface*/
 #ifndef _RBTREE_
 #define _RBTREE_
 
 
 #endif
-
+/*AVL Tree Interface*/
 #ifndef _AVLTREE_
 #define _AVLTREE_
 
 
 #endif
-
+/*Tries Interface*/
 #ifndef _TRIE_
 #define _TRIE_
 
