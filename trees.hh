58831f3ad5465315bc9ccb6814bbb901838f9c8b
Delveri Munang <chickdelveri@gmail.com>
2015-08-10 08:44:32 +0100

    File guards for AVL and TRIES

---
+++ trees.hh
@@ -0,0 +1,26 @@
+#ifndef _BTREE_
+#define _BTREE_
+
+
+
+#endif
+
+#ifndef _RBTREE_
+#define _RBTREE_
+
+
+#endif
+
+#ifndef _AVLTREE_
+#define _AVLTREE_
+
+
+#endif
+
+#ifndef _TRIE_
+#define _TRIE_
+
+
+
+#endif
+
