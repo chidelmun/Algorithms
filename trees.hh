ceae2234b2f473856b332d5d58da8dc26bd98638
Delveri Chick <chickdelveri@gmail.com>
2015-11-27 05:51:52 +0100

    Print all paths from lroot node to leaf nodes

--- trees.hh
+++ trees.hh
@@ -178,6 +178,43 @@ int has_path_sum(struct node* root, int sum){
 	}
 }
 
+void printPaths(struct node* node) { 
+  int path[1000];
+  printPathsRecur(node, path, 0); 
+}
+
+/* 
+ Recursive helper function -- given a node, and an array containing 
+ the path from the root node up to but not including this node, 
+ print out all the root-leaf paths. 
+*/ 
+void printPathsRecur(struct node* node, int path[], int pathLen) { 
+  if (node==NULL) return;
+
+  // append this node to the path array 
+  path[pathLen] = node->data; 
+  pathLen++;
+
+  // it's a leaf, so print the path that led to here 
+  if (node->left==NULL && node->right==NULL) { 
+    printArray(path, pathLen); 
+  } 
+  else { 
+  // otherwise try both subtrees 
+    printPathsRecur(node->left, path, pathLen); 
+    printPathsRecur(node->right, path, pathLen); 
+  } 
+}
+
+// Utility that prints out an array on a line. 
+void printArray(int ints[], int len) { 
+  int i; 
+  for (i=0; i<len; i++) { 
+    printf("%d ", ints[i]); 
+  } 
+  printf("\n"); 
+} 
+
 #endif
 /*Red Black Tree Interface*/
 #ifndef _RBTREE_
