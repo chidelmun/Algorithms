717f8ae4a519217eb4229fa485952d5f23626d4f
Delveri Chick <chickdelveri@gmail.com>
2015-11-27 20:02:29 +0100

    Count trees generic

--- trees.hh
+++ trees.hh
@@ -250,7 +250,33 @@ int same_tree(struct node* tree_one, struct node* tree_two){
 				and (same_tree(tree_one->right, tree_two->right)));
 		}
 	}
+
 }
+
+int countTrees(int numKeys) {
+
+  if (numKeys <=1) { 
+    return(1); 
+  } 
+  else { 
+    // there will be one value at the root, with whatever remains 
+    // on the left and right each forming their own subtrees. 
+    // Iterate through all the values that could be the root... 
+    int sum = 0; 
+    int left, right, root;
+
+    for (root=1; root<=numKeys; root++) { 
+      left = countTrees(root - 1); 
+      right = countTrees(numKeys - root);
+
+      // number of possible trees with this root == left*right 
+      sum += left*right; 
+    }
+
+    return(sum); 
+  } 
+}
+
 
 #endif
 /*Red Black Tree Interface*/
