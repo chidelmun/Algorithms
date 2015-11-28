ae772e55048940b2c7a7e4315c2d83c50e120dea
Delveri Chick <chickdelveri@gmail.com>
2015-11-28 20:03:10 +0100

    Generic checks for BST

--- trees.hh
+++ trees.hh
@@ -277,6 +277,28 @@ int countTrees(int numKeys) {
   } 
 }
 
+int isBST2(struct node* node) { 
+  return(isBSTUtil(node, INT_MIN, INT_MAX)); 
+}
+/* 
+ Returns true if the given tree is a BST and its 
+ values are >= min and <= max. 
+*/ 
+int isBSTUtil(struct node* node, int min, int max) { 
+  if (node==NULL) return(true);
+
+  // false if this node violates the min/max constraint 
+  if (node->data<min || node->data>max) return(false);
+
+  // otherwise check the subtrees recursively, 
+  // tightening the min or max constraint 
+  return 
+    isBSTUtil(node->left, min, node->data) && 
+    isBSTUtil(node->right, node->data+1, max) 
+  ); 
+} 
+ 
+
 
 #endif
 /*Red Black Tree Interface*/
